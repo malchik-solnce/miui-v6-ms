c0e6f77b8367bb8abd798ad19feaad0fa156077d
Сережа <solncemalchik@gmail.com>
2014-09-27 12:43:03 +0200

    Fixed+MiBand

---
+++ translation/com.xiaomi.hm.health/res/raw/localization_english.lua
@@ -0,0 +1,73 @@
+-------------------------
+-- English Localization String resources
+-- Huami Co, ltd. (2014)
+-- Herbert dai
+-- 20140731
+-------------------------
+localization_English_table = {
+    calories_table={
+
+    },
+
+    ok='OK',
+
+    hour='ч.',
+    minute='мин.',
+    month='month',
+    day='day',
+    step="step",
+    km='км',
+    minute_format='%d мин.',
+    hour_minute_format='%d ч. %d мин.',
+    hour_format='%d ч.',
+
+    welcome_use="Привет, я Ваш браслет",
+    last_week_walked_format='За последнюю неделю Вы прошли шагов: %d',
+    last_week_walked_info_format='Пройдено %d км, сожжено %d ккал.',
+    last_month_walked_format='За последний месяц Вы прошли шагов: %d',
+    last_month_walked_info_format='Пройдено %d км, сожжено %d ккал.',
+
+    get_over_format=", about %d gram of fat.",
+    click_to_get_help="Инструкция по эксплуатации",
+    take_a_walk="Take a walk with Mi Band.",
+    take_a_walk_info="Браслет будет отслеживать вашу деятельность и поможет быть здоровыми",
+    unlock_hint="Разблокировка устройства браслетом",
+    unlock_hint_info="Скажите до свидания старым временам",
+    no_data_hint_0="Продолжайте двигаться, сделайте спорт частью своей жизни",
+    no_data_hint_1="Хотите быть счастливым? Просто запустите.",
+    no_data_hint_2="Кардио тренировки сделают вас легче и свежее каждый день! ",
+    no_data_hint_3="Нормальный взрослый человек спит 15% -20% от общего времени сна в глубоком этапе сна. ",
+    not_binded_hint="Браслет не подключен",
+    new_record_info="Ничего себе! Вы сейчас побили рекорд в ходьбе.",
+    new_record_format="Новый рекорд, %d шаг., %s %d",
+    today_goal_reached="Поздравляем! Вы достигли свою цели сегодня. ",
+    week_continue_reach_goal="You have achieved your goal every day for an entire week! What an awesome week!",
+    challenge_format="You have achieved your goal for %d days!",
+    personal_best_format="Your highest record is %d days!",
+    active_time_format_0="%d мин.",
+    active_time_format_1="%d ч.",
+    active_time_format_2="%d ч. %d мин.",
+    activie_run_format_0="Ran %s. Good job!",
+    activie_run_format_1="Ran %s. You are doing fantastic!",
+    activie_run_format_2="Ran %s, Incredible endurance!",
+    activie_run_consumed="Сожжено %d ккал. %s",
+
+    get_distance_format=", всего из %d м.",
+    activity_walk_format="%s пройдено %d шаг. %s",
+    activity_walk_consumed_format="Сожжено %d ккал. %s",
+
+    activity_activity_format="%s %s work out %s",
+    last_night_sleeped_good_format="Прошлой ночью вы спали %s. Какой отличный сон!",
+    last_night_sleeped_normal_format="Прошлой ночью вы спали %s",
+    deep_sleep_format="Глубокий сон был %s",
+
+    battery_low_info="Низкое питание браслета. Поставьте на зарядку.",
+    battery_very_low_info="Подключите на зарядку",
+
+    cannot_find_bracelet="Не удается найти браслет?",
+    cannot_find_bracelet_info="Убедитесь что браслет заряжен и находится возле устройства",
+
+    challenge_to_get="%s days from making a new record!",
+    record_reach_max="You have hit your highest record!",
+    new_record_born="Новый рекорд!",
+}
